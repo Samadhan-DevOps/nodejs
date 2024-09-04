96de301aab92c39a33029021c12f795cf20c9009
samadhan <samadhandhumal1234@gmail.com>
2024-09-04 07:10:41 +0000

    hii

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
  
 // Define a route for the root URL
 app.get('/', (req, res) => {
-  res.send('my, World!');
+  res.send('hello, World!');
 });
  
 // Define the port number and start the server
